d68a9ad655dcf7e428563ea1140d25a0dd8b7f05
dorker6 <73139073+dorker6@users.noreply.github.com>
2022-01-04 11:01:49 +0100

    Color support(base code)

--- compile.sh
+++ compile.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env sh 
 FILE="simttylogin"
 
-cc  `pkg-config --cflags --libs pam` -O2 -std=c99 -lpam_misc -o $FILE $FILE.c
+CPPFLAGS=""
+
+#POSITIONAL_ARGS=()
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -c|--enable-color)
+      CPPFLAGS=$CPPFLAGS-DCOLOR
+      shift 
+      ;;
+    -*|--*)
+      printf "%s:Unknown argument:\"%s\"\n" $0 $1 || echo -e "$0: Uknown argument:\"$1\""
+      exit 1
+      ;;
+    #*)
+      #POSITIONAL_ARGS+=("$1") 
+      #shift 
+     # ;; I won't plan to  use this (because this requires bash)
+  esac
+done
+cc  `pkg-config --cflags --libs pam` -O2 -std=c99 -lpam_misc $CPPFLAGS -o $FILE $FILE.c
